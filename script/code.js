7b016d284dd295eaf1969dccab7104d32b0febb6
Muttaqeen-Slamat <muttaqeens@gmail.com>
2023-11-27 09:17:59 +0200

    patch

---
+++ script/code.js
@@ -0,0 +1,16 @@
+const conversion = document.querySelector('[data-submit]')
+const results = document.querySelector('#output') 
+let arr = [0,1,2,3,4,5,6,7,8,9,10]
+
+function getting (){
+if(number in arr){
+    let input =
+}
+}
+// function value (){
+//     let minutes = document.getElementById('minutes').value
+//     let seconds = minutes * 60
+//     results.textContent = seconds.toFixed(2)
+// }
+
+// conversion.addEventListener('click',value)
